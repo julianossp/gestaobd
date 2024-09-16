207f7084a3366ff54fe3daf2a69a31cd7a354f6f
julianossp <98223129+julianossp@users.noreply.github.com>
2024-09-16 16:53:18 -0300

    Atualiza dados

--- procedure_Processa_Comissionamento.sql
+++ procedure_Processa_Comissionamento.sql
@@ -1,72 +1,118 @@
-CREATE DEFINER=`root`@`%` PROCEDURE `processa_comissionamento`(
-	IN data_inicial DATE,
-	IN data_final DATE,
-	OUT total_processado INT)
+DROP PROCEDURE if EXISTS processa_comissionamento;
+# --------------------------------------------------
+delimiter $$
+create procedure processa_comissionamento(
+in  data_inicial     date,
+in  data_final       date  ,
+out total_processado int )
 BEGIN
-		DECLARE total_venda FLOAT(10,2) DEFAULT 0;
-		DECLARE venda INT DEFAULT 0;
-		DECLARE vendedor INT DEFAULT 0;
-		DECLARE comissao FLOAT(10,2) DEFAULT 0;
-		DECLARE valor_comissao FLOAT(10,2) DEFAULT 0;
-		DECLARE aux INT DEFAULT 0;
-		DECLARE fimloop INT DEFAULT 0;
-/*
-cursor para buscar os registros a serem processados entre a data inicial e data final e valor total de venda é maior que zero.
-*/		
-		DECLARE busca_pedido CURSOR FOR
-			SELECT n_numevenda, n_totavenda, n_numevende
-			FROM comvenda
-			WHERE d_datavenda BETWEEN data_inicial 
-			AND data_final AND n_totavenda > 0;
-	/* rotina de tratamento para o banco de dados não executar o loop quando ele terminar, evitando que retorne erro.
-*/
-		DECLARE CONTINUE handler FOR SQLSTATE '02000' SET fimloop = 1;
-# Inicio do loop
-	 	vendas: LOOP
-		#verifica se o loop terminou e saio do loop
-		if fimloop = 1 then 
-			leave vendas;
-		END if;	
-		#recebe o resultado da consulta em cada variável
-		fetch busca_pedido INTO venda, total_venda, vendedor;
-		#busca o valor do percentual de cada vendedor
-		SELECT n_porcvende INTO comissao
-		FROM comvende
-		WHERE n_numevende = vendedor;
-		#verifica se o percentual do vendedor é maior
-		#que zero logo após a condição deve ter o then
-		if (comissao > 0) then
-			##calcula o valor da comissão
-			SET valor_comissao =  ((total_venda * comissao) /100);
-			#atualiza (update) a tabela comvenda com o valor da comissão
-			UPDATE comvenda SET n_vcomvenda = valor_comissao WHERE n_numevenda = venda;
-			COMMIT;
-			
-/*verifica se o percentual do vendedor é igual a zero na regra do nosso sistema se o vendedor tem 0 ele ganha 0 porcento de comissão 
-*/
-		ELSEIF (comissao= 0) then
-			UPDATE comvenda SET n_vcomvenda = 0 
-			WHERE n_numevenda = venda;
-			COMMIT;
-/* se o vendedor nao possuir registro no percentual de comissao ele irá ganhar 1 de comissao isso pela regra de negocio do sistema.
-*/
-	ELSE 
-		SET comissao = 1;
-		SET valor_comissao = ((total_venda * comissao) /100);
-		UPDATE comvenda SET n_vcomvenda = valor_comissao WHERE n_numevenda = venda;
-		COMMIT;
-		#encerra o if
-	END if;
+# Declara as variáveis
+declare total_venda    float(10,2) default 0;
+declare venda          int         default 0;
+declare vendedor       int         default 0;
+declare comissao       float(10,2) default 0;
+declare valor_comissao float(10,2) default 0;
+declare aux            int         default 0;
+declare fimloop        int default 0;
+
+## gera o cursor para buscar os registros a serem 
+## processados entre a data inicial e data final
+## e valor total de venda é maior que zero
+declare busca_pedido cursor for 
+	select n_numevenda,
+	       n_totavenda,
+		   n_numevende
+	  from comvenda
+	 where d_datavenda between data_inicial 
+	 	and data_final
+	  	and n_totavenda > 0 ;
+
+## rotina para o banco não 
+## executar o loop quando ele terminar
+## evitando que retorne qualquer erro
+declare 
+continue handler 
+for sqlstate '02000' 
+set fimloop  = 1;
+
+
+## abre o cursor				
+open busca_pedido;
 	
-				set comissao = 0;
-				##utilizo a variável aux para contar a quantidade
-				set  aux = aux +1 ;
-			end loop vendas;
-				## atribuo o total de vendas para a variável de
-				## saída
-			set total_processado = aux;
-			## fecho o cursor
-			close busca_pedido;
+	## inicia do loop
+	vendas: LOOP
+	
+	## Verifica que se o loop terminou
+	## e sai do loop
+	if fimloop  = 1 then 
+	  leave  vendas;
+	end if;
+	
+	##recebe o resultado da consulta em cada variável
+	fetch busca_pedido into venda, total_venda, 
+	vendedor;
+	
+	## busca o valor do percentual de cada vendedor
+	select n_porcvende 
+      into comissao 
+      from comvende
+     where n_numevende = vendedor;
+	
+	## verifica se o percentual do vendedor é maior
+	## que zero logo após a condição deve ter o then
+	if (comissao > 0 ) then 
+	    ## calcular o valor da comissao
+		set valor_comissao  = 
+			((total_venda * comissao)  / 100);
 		
-			##retorno o total de vendas processadas
- 	END
+		## faz o update na tabela comvenda com o 
+		## valor da comissão
+		update comvenda set 
+		n_vcomvenda = valor_comissao
+		where n_numevenda = venda;
+		commit; 
+	
+	## verifica se o percentual do vendedor é igual 
+	## zero na regra do nosso sistema se o vendedor 
+	## tem 0 ele ganha 0 porcento de comissão
+       elseif(comissao = 0) then
+	    
+		update comvenda set n_vcomvenda = 0
+		where n_numevenda = venda;
+		commit;
+	
+	## se ele não possuir registro no percentual de 
+    ## comissão ele irá ganhar 1 de comissão
+	## isso pela regra de negócio do nosso sistema
+	else 
+		set comissao = 1;
+		set valor_comissao = 
+			((total_venda * comissao)  / 100);
+		
+		update 
+			comvenda set n_vcomvenda = valor_comissao
+		where n_numevenda = venda;
+		commit;  
+	## fecho o if	 
+	end if;
+	
+	set comissao = 0;
+	##utiliza a variável aux para contar a quantidade
+	set  aux      = aux +1 ;
+end loop vendas;
+	## atribuir o total de vendas para a variável de
+	## saída
+set total_processado = aux;
+## fecha o cursor
+close busca_pedido;
+
+##retorna o total de vendas processadas
+
+
+END
+$$
+
+#--------------------------------------
+
+call processa_comissionamento('2015-01-01','2015-05-30' ,@a);
+select @a;
